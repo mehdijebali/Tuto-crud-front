7d7741232842824559b50484d379094e74111944
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-03-12 16:33:27 +0100

    Update baseurl

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://192.168.49.2:30100/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
