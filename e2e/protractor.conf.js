641b01985cbcf537d9b8141333702d5d49e51454
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-03-12 15:55:57 +0100

    Add localhost

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://192.168.49.2:30100/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
